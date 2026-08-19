3d77917c002690a5de5b6ae352a2b78be4b965a9
Vyzarts <vyzartsstudios@gmail.com>
2026-08-19 13:35:32 -0600

    refactor: rewrite conversion methods and update switch cases

--- Tarea1_binario.java
+++ Tarea1_binario.java
@@ -1,47 +1,65 @@
-package Tarea1_binario;
-
 import java.util.Scanner;
 
 public class Tarea1_binario {
 
-    public static void main(String[] args){
-                Scanner Sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
 
         System.out.println("Selecciona un sistema numerico:\n");
         System.out.println("1. Binario");
         System.out.println("\n2. Decimal");
         System.out.println("\n3. Hexadecimal");
+        int opcion = Sc.nextInt();
 
-        int opcion =  Sc.nextInt();
-
-
-        switch(opcion){
+        switch (opcion) {
             case 1:
-                Binario();
-            break;
+                System.out.println("dame El numero binario: ");
+                int binario = Sc.nextInt();
+
+                Decimal();
+                Hexadecimal();
+
+                break;
 
             case 2:
+
+                System.out.println("dame El numero Decimal: ");
+                int decimal = Sc.nextInt();
+
                 Decimal();
-            break;
+                Hexadecimal();
+
+                break;
 
             case 3:
-                Hexadecimal();
-            break;
+
+                System.out.println("dame El numero Hexadecimal: ");
+                int Hexadecimal = Sc.nextInt();
+
+                Binario();
+                Decimal();
+
+                break;
 
             default:
                 System.out.println("matate");
         }
+        Sc.close();
     }
 
-    private static void Binario(){
+    private static void Binario() {
 
     }
 
-    private static void Decimal(){
-        
+    private static void Decimal() {
+
+        for (int i = opcion; i >= 1; i++) {
+
+        }
+
     }
 
-    private static void Hexadecimal(){
+    private static void Hexadecimal() {
 
     }
 }
