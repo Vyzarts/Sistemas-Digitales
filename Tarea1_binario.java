7c3f4f20b8b5df7ed701e2721d324cb548bdd104
Vyzarts <vyzartsstudios@gmail.com>
2026-08-21 19:38:00 -0600

    feat: add decimal to hex and binary conversion logic
    - Updated 3 constructors with logic for dec to hex and dec to bin.
    
    - WIP: Binary to decimal converter is currently incomplete.

--- Tarea1_binario.java
+++ Tarea1_binario.java
@@ -16,8 +16,8 @@ public class Tarea1_binario {
                 System.out.println("dame El numero binario: ");
                 int binario = Sc.nextInt();
 
-                Decimal();
-                Hexadecimal();
+                Decimal(binario);
+                Hexadecimal(binario);
 
                 break;
 
@@ -26,8 +26,8 @@ public class Tarea1_binario {
                 System.out.println("dame El numero Decimal: ");
                 int decimal = Sc.nextInt();
 
-                Decimal();
-                Hexadecimal();
+                Binario(decimal);
+                Hexadecimal(decimal);
 
                 break;
 
@@ -36,8 +36,8 @@ public class Tarea1_binario {
                 System.out.println("dame El numero Hexadecimal: ");
                 int Hexadecimal = Sc.nextInt();
 
-                Binario();
-                Decimal();
+                Binario(Hexadecimal);
+                Decimal(Hexadecimal);
 
                 break;
 
@@ -47,19 +47,84 @@ public class Tarea1_binario {
         Sc.close();
     }
 
-    private static void Binario() {
+    private static void Binario(int numero) {
 
+        String resultado = "" ;
+
+        int numeroOR = numero;
+
+        while(numero > 0){
+            
+            if(numero % 2 == 0){
+                resultado = 0 + resultado;
+            }else{
+                resultado = 1 + resultado;
+            }
+            numero = numero / 2;
+        }
+        System.out.println("el "+numeroOR+" en binario es: "+ resultado);
     }
 
-    private static void Decimal() {
+    private static void Decimal(int numero) {
+            // Convertir de decimal a binario y hexadecimal
+            /**decimal a binario
+             * agarramos el numero de derecha a izquierda, a cada uno lo multiplicamos por 2**(n) donde por cada nuevo numero n = n +1**/
+        String resultado = "";
+            //0001
+            int residuo = numero % 10;
+            int posicion = 0;
 
-        for (int i = opcion; i >= 1; i++) {
-
+        while(numero > 0){
+            
         }
 
+
     }
 
-    private static void Hexadecimal() {
+    private static void Hexadecimal(int numero) {
+        
+        String resultado = "";
+        
+        int numeroOR = numero;
+
+
+        while(numero > 0){
+            
+            int sobrante = (numero % 16);
+
+            if(sobrante >= 10){
+
+                switch (sobrante) {
+                    case 10:
+                        resultado = "A" + resultado;
+                        break;
+                    case 11:
+                        resultado = "B" + resultado;
+                        break;
+                    case 12:
+                        resultado = "C" + resultado;
+                        break;
+                    case 13:
+                        resultado = "D" + resultado;
+                        break;
+                    case 14:
+                        resultado = "F" + resultado;
+                        break;
+                    case 15:
+                        resultado = "G" + resultado;
+                        break;
+                    default:
+                        break;
+                }
+
+            }else{
+
+                resultado = sobrante + resultado;
+
+            }
+            numero = numero / 16;  
+        }
+            System.out.println("el "+ numeroOR +" en hexadecimal es: " + resultado); 
 
     }
 }
